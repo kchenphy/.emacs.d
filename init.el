ac9ed09de1b13a3565ea1e70c2052f59f1ce291a
Kuang Chen <kuangchen@google.com>
2020-08-08 22:42:24 -0700

    Initial commit.

---
+++ init.el
@@ -0,0 +1,12 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(unless package--initialized (package-initialize))
+
+(require 'org)
+
+(setq kuantum/org-init-file "~/.emacs.d/emacs-init.org")
+(org-babel-load-file (expand-file-name kuantum/org-init-file))
